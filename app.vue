909cec5e700c54fc69b23f532cdadde7b69893da
jia-ayo <milkjia11@gmail.com>
2023-04-26 23:37:01 +0100

    left icons

--- app.vue
+++ app.vue
@@ -1,24 +1,32 @@
 <template>
-  <div :class="{ 'dark': darkMode }">
+  <div :class="{ dark: darkMode }">
     <div class="bg-white dark:bg-dim-900">
       <div class="min-h-full">
-        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 md:gap-5 lg:gap-9"
+        >
           <!-- left  sidebar -->
-          <div class="hidden  md:block xs-col-span-1 xl:col-span-2">
+          <div class="hidden md:block xs-col-span-1 xl:col-span-2 m-1">
             <div class="sticky top-0">
               <SidebarLeft />
             </div>
-
           </div>
           <!-- Main content -->
+          <main class="col-span-10 bg-red-500 lg:col-span-7 xl:col-span-6">
+            <h1>asd</h1>
+          </main>
           <!-- right sidebar -->
+          <div class="hidden md:block xl:col-span-4 md:col-span-3 bg-blue-500">
+            <div class="sticky top-0">
+              <h1>Right Sidebar</h1>
+            </div>
+          </div>
         </div>
-      </div>
+      </div> 
     </div>
   </div>
 </template>
 
 <script setup>
-const darkMode = ref(true);
+const darkMode = ref(false);
 </script>
- 
---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,28 @@
+<template>
+  <NuxtLink
+    to="#"
+    class="flex items-center p-3 text-black  rounded-full w-min hover:bg-gray-200 dark:hover:bg-dim-200 dark:text-white"
+    :class="defaultTransition"
+  >
+    <div class="w-5 h-5 text-dark">
+      <slot name="icon"></slot>
+    </div>
+    <div class="hidden lg:hidden xl:block ml-4 text-lg " :class="textClasses">
+      <slot name="name"></slot>
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup>
+const { defaultTransition } = useTailwindConfig();
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const textClasses = computed(() =>
+  props.active ? "font-semibold" : "font-normal"
+);
+</script>
